a3ae242c7ed3f4e7bcb72700c8f50b279ccdeb54
IkkonM <IkkonM@gmail.com>
2021-11-11 18:09:17 +0100

    test plik c#

---
+++ program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Projekt_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
