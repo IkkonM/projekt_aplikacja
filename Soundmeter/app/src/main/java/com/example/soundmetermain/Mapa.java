f44c15bc267ae648d8c660714e1b4ff4cf042676
IkkonM <IkkonM@gmail.com>
2021-11-21 21:37:18 +0100

    Projekt w Android Studio

---
+++ Soundmeter/app/src/main/java/com/example/soundmetermain/Mapa.java
@@ -0,0 +1,62 @@
+package com.example.soundmetermain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Mapa extends AppCompatActivity {
+    private Button buttonA;
+    private Button buttonB;
+    private Button buttonC;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mapa);
+
+
+        buttonC = (Button) findViewById(R.id.buttonC);
+        buttonC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity3();
+            }
+        });
+
+
+        buttonA = (Button) findViewById(R.id.buttonA);
+        buttonA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity1();
+            }
+        });
+
+        buttonB = (Button) findViewById(R.id.buttonB);
+        buttonB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity0();
+            }
+        });
+
+    }
+
+    private void openActivity3() {
+        Intent intent = new Intent(this, Ustawienia.class);
+        startActivity(intent);
+    }
+
+    private void openActivity1() {
+        Intent intent = new Intent(this, Mapa.class);
+        startActivity(intent);
+    }
+    private void openActivity0() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
+
